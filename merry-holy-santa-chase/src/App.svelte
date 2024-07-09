99ec685953275fbed19b969938d980d5d25a194d
Jan Seipel <janfseipel@gmail.com>
2024-07-09 23:05:40 +0200

    added firestore connection

--- merry-holy-santa-chase/src/App.svelte
+++ merry-holy-santa-chase/src/App.svelte
@@ -1,15 +1,18 @@
 <script>
 
   // === todos ===
-  // Emojis durch SVG-Grafiken ersetzen
-  // Backend hinzufügen
-  // Datenbank anschließen
+  // Datebank gegen Löschen absichern
+  // Fehlende Top-Ten-Plätze durch Platzhalter ersetzen
+  // Styling von Counter und Countdown anpassen
+  // Speed-Up-Interval ändern
   // Spielanleitung hinzufügen (evtl. mit Cookie?)
   // Impressum mit Credits hinzufügen
   // Username auf 10 Zeichen begrenzen
   
   // === bugs ===
   // Spielfeld tatsächlich responsive machen
+  // Namenlose werte von der Datenbank ausschließen
+  // mehrfaches Eintragen ohne neues Spiel vermeiden (z.B. durch einführen eines Wahrheitswerte im Store scoreInDb = false/true)
 
   import Router from 'svelte-spa-router'
   import Home from "./lib/Home.svelte"
--- merry-holy-santa-chase/src/lib/Gameover.svelte
+++ merry-holy-santa-chase/src/lib/Gameover.svelte
@@ -2,13 +2,25 @@
 
     import {push} from "svelte-spa-router"
     import {get} from "svelte/store"
-    import {CountStore, PlayerName, IsTopTen, Scores} from "./store.js"
+    import {CountStore, PlayerName} from "./store.js"
     import Ranking from "./Ranking.svelte"
+    import {initializeApp} from "firebase/app";
+    import {getFirestore, addDoc, collection, getDocs} from "firebase/firestore"
+
+    const firebaseConfig = {
+        apiKey: "AIzaSyBpOgtLkUIXsLaHmqZHoqVV_0D3dCfVlVk",
+        authDomain: 'merry-holy-santa-chase.firebaseapp.com',
+        projectId: 'merry-holy-santa-chase',
+        storageBucket: 'merry-holy-santa-chase.appspot.com',
+        messagingSenderId: '1050325198070',
+        appId: '1:1050325198070:web:2efd1e961bf1c135af38d7',
+    };
+
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
     const name = get(PlayerName)
-    const count = get(CountStore)
-    // const isTopTen = get(IsTopTen);
-    let scores = get(Scores);
+    const count = get(CountStore) 
     const gameResult = {"name": name, "score": count, "new": true}
     const restart = (answer) => {
         CountStore.set(0)
@@ -19,22 +31,44 @@
             push("/")
         }
     }
+    
+    const getDataFromDb = async () => {
 
-    scores = [...scores, gameResult]
-    const topten = scores.sort((a,b) => {return b.score - a.score}).slice(0,10)
-    const isTopTen = topten.some(e=>e.new === true)
+        // get scores from database
+        const querySnapshot = await getDocs(collection(db, "userscores"));
+        const userScores = querySnapshot.docs.map((d) => d.data())
+        console.log(userScores)
+
+        // compute top ten high score
+        const scores = [...userScores, gameResult]
+        const topTen = scores.sort((a,b) => {return b.score - a.score}).slice(0,10)
+        const isTopTen = topTen.some(e=>e.new === true)
+
+        // write game result to database
+        const docRef = await addDoc(collection(db, "userscores"), {...gameResult, "new": false})
+        console.log("Game result written to database with ID: ", docRef.id);
+
+        return {topTen, isTopTen}
+    }
 
 </script>
 
 <div>
-    {#if isTopTen}
-        <h1>Super {name}!</h1>
-        <p>Mit deinen {count} Punkten hast du es in die Top-Ten aller Spieler_innen geschafft.</p>
-    {:else}
-        <h1>Schade {name}</h1>
-        <p>Du hast {count} Punkte erreicht. Das reicht leider nicht für die aktuelle Top-Ten.</p>
-    {/if}
-    <Ranking gameResult={gameResult} topten={topten}/>
+
+    {#await getDataFromDb()}
+        ... warte auf das Resultat
+    {:then result}
+        {#if result.isTopTen}
+            <h1>Super {name}!</h1>
+            <p>Mit deinen {count} Punkten hast du es in die Top-Ten aller Spieler_innen geschafft.</p>
+        {:else}
+            <h1>Schade {name}</h1>
+            <p>Du hast {count} Punkte erreicht. Das reicht leider nicht für die aktuelle Top-Ten.</p>
+        {/if}
+        <Ranking gameResult={gameResult} topten={result.topTen}/>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await }
     <p>Möchtest du es nochmal versuchen?</p>
     <button on:click={()=>restart("yes")}>Ja</button>
     <button on:click={()=>restart("no")}>Nein</button>
