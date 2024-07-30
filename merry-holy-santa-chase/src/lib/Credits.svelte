56dd9798cc87f1c1f61e17b9b835860233a2d4b4
Jan Seipel <janfseipel@gmail.com>
2024-07-30 21:37:40 +0200

    edited credits

--- merry-holy-santa-chase/src/lib/Credits.svelte
+++ merry-holy-santa-chase/src/lib/Credits.svelte
@@ -8,16 +8,16 @@
         <p>Jani, der kleine Weihnachtswichtel</p>
     </div>
     <div>
-        <p>MUSIK:<p/>
-        <p><a href="https://freemusicarchive.org/music/Nullsleep/The_8bits_of_Christmas/8bp038-02-nullsleep-silent_night/">Silent night</a> by Nullsleep. (Lizenz: <a href="https://creativecommons.org/licenses/by-nc-nd/3.0/">CC BY-NC-ND 3.0</a>)</p>
+        <p>MUSIK UND SOUNDEFFEKTE:<p/>
+        <p>"Stille Nacht" und anderer Sound-Trash von SantaSeipel via <a href="https://www.beepbox.co/">BeepBox</a> </p>
     </div>
     <div>
         <p>SCHRIFTART:</p>
-        <p><a href="https://www.1001fonts.com/kongtext-font.html">Kongtext</a> by codeman38. (Lizenz: <a href="https://st.1001fonts.net/license/kongtext/license.txt">custom</a>)</p>
+        <p><a href="https://www.1001fonts.com/kongtext-font.html">Kongtext</a> von codeman38. (Lizenz: <a href="https://st.1001fonts.net/license/kongtext/license.txt">hier</a>) - <br />Ä,Ö und Ü hat allerdings der Weihnachtsmann gebracht ;)</p>
     </div>
     <div>
         <p>EMOJIS:</p>
-        <p><a href="https://openmoji.org/">OpenMoji</a> changed to Pixelart. (Lizenz: <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>)</p>
+        <p><a href="https://openmoji.org/">OpenMoji</a> (Lizenz: <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a>) - <br />durch ein Weihnachtswunder im Pixelart-Design.</p>
     </div>
     <button on:click={()=>push("/")}>Zurück zum Start</button>
 </div>
